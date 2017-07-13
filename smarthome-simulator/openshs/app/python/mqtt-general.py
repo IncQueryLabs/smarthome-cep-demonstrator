8427c05a878c8547f95c73dbb23bb9ad89d7e5a4
dandrid <dandrid2@gmail.com>
2017-07-13 17:39:40 +0200

    OpenSHS MQTT fix + improvements

--- smarthome-simulator/openshs/app/python/mqtt-general.py
+++ smarthome-simulator/openshs/app/python/mqtt-general.py
@@ -1,32 +1,17 @@
-from bge import logic
-import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
+import mymqtt
 
 cont = logic.getCurrentController()
 own = cont.owner
+parent = own.parent
 
-def on():
-    
-def off():
-    
 
-def mqtt_publish(message):
-    publish.single("in/"+own.name+"/command", payload=message, qos=0, retain=False, hostname="127.0.0.1",
-       port=1183, client_id="python-client", keepalive=60, will=None, auth=None,
-       tls=None, protocol=mqtt.MQTTv31)
 
-def on_message(client, userdata, msg):
-    if msg.payload == b'ON':
-        on()
-    elif msg.payload == b'OFF':
-        off()
+def callback_message(msg):
+    if msg == b'ON':
 
-client = mqtt.Client()
-client.on_message = on_message
-client.connect("127.0.0.1", 1183, 60)
-client.subscribe("out/"+own.name+"/state")
+    elif msg == b'OFF':
+        
+client = mymqtt.MyClient(own.name, callback)
 
-client.loop_start()
-
-mqtt_publish("ON")
-mqtt_publish("OFF")
+client.mqtt_publish("ON")
+client.mqtt_publish("OFF")
