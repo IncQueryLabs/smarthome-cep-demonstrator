c149156b813a96d3f119f98b538dd18029a53c73
András Jankó <dandrid2@gmail.com>
2017-08-14 15:53:02 +0200

    Drools wrapper

---
+++ esh-event-bus/com.incquerylabs.iot.droolswrapper/src/main/java/com/incquerylabs/iot/droolswrapper/App.java
@@ -0,0 +1,13 @@
+package com.incquerylabs.iot.droolswrapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
