c2b651172a88fc08511e398134ae75292fe758e4
András Jankó <dandrid2@gmail.com>
2017-09-18 00:16:45 +0200

    HomeIO MQTT C#

---
+++ smarthome-simulator/HomIO-MQTT/HomeIO-MQTT/Program.cs
@@ -0,0 +1,29 @@
+﻿using EngineIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace HomeIO_MQTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeIO homeIO = new HomeIO();
+            Console.WriteLine("Press any key to exit...");
+
+            while (!Console.KeyAvailable)
+            {
+                homeIO.Update();
+               Thread.Sleep(16);
+            }
+            homeIO.Dispose();
+        }
+    }
+}
